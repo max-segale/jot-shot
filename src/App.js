8aeb51eef7b0199cd83d6094361b405a2c3ae8db
Max Segale <maxsegale@gmail.com>
2020-04-29 21:06:25 -0400

    Starting build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
+
+class View extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleClick() {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className='view' onClick={this.handleClick}>
+        <canvas></canvas>
+      </div>
+    );
+  }
+}
+
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='menu'>
+        <ul>
+          <li>Text</li>
+          <li>Photo</li>
+        </ul>
+      </div>
+    );
+  };
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='wrapper'>
+      <header>
+        <h1 className='heading'>Jot Shot</h1>
       </header>
+      <View />
+      <Menu />
+      <footer>
+      </footer>
     </div>
   );
 }
